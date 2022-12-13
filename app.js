0ebfbff886bab5f35b60df6789f6e3e5ab0dcb64
Rokas <rookas.rudzenskas@gmail.com>
2022-12-13 14:24:45 +0200

    Add a card to the page
    This is a card that will be used to display the content of the page.

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 gsap.registerPlugin(Flip);
 
 const links = document.querySelectorAll('.nav-item a');
-const active = document.querySelector('.active-nav');
+const activeNav = document.querySelector('.active-nav');
 
 links.forEach((link) => {
-    link.addEventListener('click', () => {
+    link.addEventListener('click', (e) => {
+        // Turn navs to blue color
         gsap.to(links, { color: "#252525"});
-    })
+        if(document.activeElement === link) {
+            gsap.to(link, {color: "#385ae0"});
+        }
+
+        // Moving the line
+        const state = Flip.getState(activeNav);
+        link.appendChild(activeNav);
+        Flip.from(state, {
+            duration: 0.5,
+            absolute: true,
+        })
+    });
 })
