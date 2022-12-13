e589bc016680b0db3bcf3ee9d763406776e8babc
Rokas <rookas.rudzenskas@gmail.com>
2022-12-13 15:25:42 +0200

    Add cards
    - Add cards
    - Add card flip animation

--- app.js
+++ app.js
@@ -21,3 +21,32 @@ links.forEach((link) => {
         })
     });
 })
+
+// Cards
+
+const cards = document.querySelectorAll('.card');
+cards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        const state = Flip.getState(cards)
+
+        const isCardActive = card.classList.contains('active');
+        cards.forEach((otherCard, otherIdx) => {
+            otherCard.classList.remove('active');
+            otherCard.classList.remove('is-inactive');
+            if(!isCardActive && index !== otherIdx) {
+                otherCard.classList.add('is-inactive');
+            }
+        });
+        if(!isCardActive) {
+            card.classList.add('active');
+        }
+        Flip.from(state, {
+            duration: 1,
+            ease: 'expo.out',
+            absolute: true,
+            onComplete: () => {
+                gsap.to('.card p', {})
+            }
+        })
+    })
+})
